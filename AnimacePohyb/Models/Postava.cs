13531f397efcc8870c47e0ef8b1afb698a8f3661
Zdeněk Zálešák <zalesakz@office365.zcu.cz>
2024-04-15 18:20:51 +0200

    Přidejte soubory projektu.

---
+++ AnimacePohyb/Models/Postava.cs
@@ -0,0 +1,21 @@
+﻿namespace AnimacePohyb.Models
+{
+    public class Postava
+    {
+        public Postava(string obrazek, string popisek) 
+        {
+            Obrazek = obrazek;
+            Popisek = popisek; 
+        }
+        private List<Souradnice> PoziceList { get; set; } = new List<Souradnice>();
+        public string Obrazek { get; private set; }
+        public int AktualniPozice { get; set; }
+        public string Popisek { get; private set; }
+        public string Style => PoziceList[AktualniPozice].Style;
+        public void PridejPozici(int pozX, int pozY, int velikostObrazku)
+        {
+            var souradnice = new Souradnice(pozX, pozY, velikostObrazku);
+            PoziceList.Add(souradnice);
+        }
+    }   
+}
---
+++ AnimacePohyb/Pages/Animace1.razor.cs
@@ -0,0 +1,24 @@
+﻿using AnimacePohyb.Models;
+
+namespace AnimacePohyb.Pages
+{
+    public partial class Animace1
+    {
+        public Postava Postava { get; private set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            InicializaceHry();
+        }
+
+        private void InicializaceHry()
+        {
+            Postava = new Postava(obrazek: "../img/maxipes.png", popisek: "Maxipes");
+            Postava.PridejPozici(pozX: 45, pozY: 280, velikostObrazku: 70);
+            Postava.PridejPozici(pozX: 450, pozY: 240, velikostObrazku: 40);
+            Postava.PridejPozici(pozX: 840, pozY: 340, velikostObrazku: 80);
+            Postava.PridejPozici(pozX: 110, pozY: 470, velikostObrazku: 100);
+        }
+    }
+}

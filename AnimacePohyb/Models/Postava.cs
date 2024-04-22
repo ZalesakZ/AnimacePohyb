666d774c4fa5301146f8455cea7645b7f133e4f5
Zdeněk Zálešák <zalesakz@office365.zcu.cz>
2024-04-22 18:29:55 +0200

    animace2

--- AnimacePohyb/Models/Postava.cs
+++ AnimacePohyb/Models/Postava.cs
@@ -2,20 +2,74 @@
 {
     public class Postava
     {
-        public Postava(string obrazek, string popisek) 
+        public Postava(string obrazek, string popisek, int sirkaObrazku) 
         {
             Obrazek = obrazek;
-            Popisek = popisek; 
+            Popisek = popisek;
+            SirkaObrazku = sirkaObrazku;
+            AktualniPozice = -1;
         }
         private List<Souradnice> PoziceList { get; set; } = new List<Souradnice>();
         public string Obrazek { get; private set; }
         public int AktualniPozice { get; set; }
         public string Popisek { get; private set; }
-        public string Style => PoziceList[AktualniPozice].Style;
+        private int SirkaObrazku { get; set; }
+        public string Style
+        {
+            get
+            {
+                if (AktualniPozice < 0)
+                {
+                    return $"width:{SirkaObrazku}px;";
+                }
+                else
+                {
+                    var poz = PoziceList[AktualniPozice];
+                    return poz.Style + $"width:{SirkaObrazku * poz.VelikostObrazku / 100}px;";
+                }
+                
+            }
+        }
+
+        public bool Dopredu { get; set; } = true;
+        private bool HlavaVpravo { get; set; } = true;
+        public string TransformRotateY => HlavaVpravo ? "transform: rotateY(0deg);" : "transform: rotateY(180deg);";
+        public int Progress => (int)Math.Round((double)(AktualniPozice + 1) / PoziceList.Count * 100);
         public void PridejPozici(int pozX, int pozY, int velikostObrazku)
         {
             var souradnice = new Souradnice(pozX, pozY, velikostObrazku);
             PoziceList.Add(souradnice);
+        }
+
+        public void Presun()
+        {
+            if (Dopredu)
+            {
+                if (AktualniPozice == PoziceList.Count - 1)
+                {
+                    Dopredu = false;
+                }
+            }
+            else
+            {
+                if (AktualniPozice == 0)
+                {
+                    Dopredu = true;
+                }
+            }
+
+            var predchoziPozice = AktualniPozice;
+
+            if (Dopredu)
+            {
+                AktualniPozice++;
+            }
+            else
+            {
+                AktualniPozice--;
+            }
+
+            HlavaVpravo = predchoziPozice < 0 || PoziceList[predchoziPozice].PozX < PoziceList[AktualniPozice].PozX;
         }
     }   
 }
